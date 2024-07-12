ac3c9448a95cfc9b1465ff059613fcbfa5d3de87
Rucha Khadatkar <ruchakhadatkar83@gmail.com>
2024-07-12 21:01:08 +0530

    gsap added

---
+++ index.js
@@ -0,0 +1,35 @@
+var circle = document.querySelector("#circle");
+var frame = document.querySelector(".frame");
+
+window.addEventListener("mousemove", function (dets) {
+  gsap.to(circle, {
+    x: dets.clientX,
+    y: dets.clientY,
+    duration: 0.2,
+    ease: Expo,
+  });
+});
+
+frame.addEventListener("mousemove", function (dets) {
+  gsap.to(circle, {
+    scale: 8,
+  });
+
+  gsap.to(".frame span", {
+    color: "#fff",
+    duration: 0.4,
+    y: "-5vw",
+  });
+});
+
+frame.addEventListener("mouseleave", function (dets) {
+  gsap.to(circle, {
+    scale: 1,
+  });
+  
+  gsap.to(".frame span", {
+    color: "#000",
+    duration: 0.4,
+    y: 0,
+  });
+});
